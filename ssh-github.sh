a89d4297164fba5f4de1848e3d9df898a86276f0
dl-tahir <tahir.mahmood@dictalabs.com>
2025-05-09 19:11:47 +0500

    Create ssh-github.sh

---
+++ ssh-github.sh
@@ -0,0 +1,7 @@
+ssh-keygen -t ed25519 -C "tahir.mahmood.com"
+cd .ssh/
+cat id_ed25519.pub
+echo "INFO !!! Add this key to GitHub account."
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+ssh -T git@github.com
